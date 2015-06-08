9de2afdc663256bc043f646f0ad0693e71e2a92a
Jaison Vieira <jaisonnvieira@gmail.com>
2015-06-08 20:31:23 -0300

    Added style and user controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
 
   test "should get home" do
     get :home
@@ -22,5 +19,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,10 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   # Add more helper methods to be used by all tests here...
 end
 
